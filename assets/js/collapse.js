58c1fd814a60a077f672d857da44a0b843b1cdcf
James Nagai <james.nagai@gmail.com>
2021-02-28 19:02:23 +0100

    updating

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -4,7 +4,8 @@ $(".header").click(function () {
     //getting the next element
     $content = $header.next();
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-    $content.slideToggle(500, function () {
+    //$content.slideToggle(500, function () {
+    $content.slideToggle(800, function () {
         //execute this after slideToggle is done
         //change text of header based on visibility of content div
         $header.text(function () {
